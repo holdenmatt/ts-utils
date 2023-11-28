34f06174142beb0e40d764b47bbcc2e8f5f19943
Matt Holden <holden.matt@gmail.com>
2023-11-28 10:58:12 -0700

    Add normalizeUrl

--- src/url.ts
+++ src/url.ts
@@ -9,3 +9,29 @@ export const isValidUrl = (url: string): boolean => {
     return false;
   }
 };
+
+/**
+ * Normalize a URL string.
+ *
+ * Ensure it has a valid scheme (defaulting to http:// if none is present),
+ * and remove any trailing slashes from the path.
+ *
+ * Return null if the URL is invalid.
+ */
+export const normalizeUrl = (url: string): string | null => {
+  if (!url.startsWith("http://") && !url.startsWith("https://")) {
+    url = "http://" + url;
+  }
+
+  try {
+    const parsedUrl = new URL(url);
+
+    // Remove any trailing slashes from the pathname
+    parsedUrl.pathname = parsedUrl.pathname.replace(/\/+$/, "");
+
+    return parsedUrl.toString();
+  } catch (error) {
+    // URL is invalid
+    return null;
+  }
+};
